99dc8d25e4a6d1b0232e618d8be371a8d5f72dab
uyabpras <uyabprasetyo98@gmail.com>
2024-02-14 00:19:51 +0700

    add controller createsoal2 upload doc/pdf feature

--- controllers/SoalServiceController.js
+++ controllers/SoalServiceController.js
@@ -118,7 +118,6 @@ exports.healthSoal = async (req, res) => {
         console.error('Error:', error.response ? error.response.data : error.message);
         res.status(error.response ? error.response.status : 500).json({ success: false, message: 'Error calling API', error: error.response ? error.response.data : error.message });
       }
-<<<<<<< HEAD
   };
 
 
@@ -191,6 +190,3 @@ exports.createSoal2 = async (req, res) => {
         res.status(err.response ? err.response.status : 500).json({ success: false, message: 'Error calling API', error: err.response ? err.response.data : err.message });
     }
 };
-=======
-  };
->>>>>>> cd61991dccce4091845ef34fc3d6579c27a5fe7b
